d91dceac82b03eb34eace05b8c9c09922d428d6b
Marko <holiak.m.v@gmail.com>
2024-06-05 00:36:57 +0300

    api

--- src/server.js
+++ src/server.js
@@ -1,19 +1,25 @@
-require("./config/db");
-require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const routes = require("./routes");
+require("dotenv").config();
+require("./config/db");
 
 const app = express();
 
 app.use(cors({
-  origin: 'http://healthy2-day.vercel.app',
-  credentials: true
+  origin: ['http://localhost:3000', 'https://healthy2-day-server.vercel.app'],
+  credentials: true,
+  allowedHeaders: 'Content-Type, Authorization'
 }));
 
 app.use(cookieParser());
 app.use(express.json());
 app.use(routes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 module.exports = app;
